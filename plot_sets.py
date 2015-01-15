fe99000abe09c90094a67d5f33cd97404aeb913c
Carlo De Franchis <carlodef@gmail.com>
2015-01-15 16:35:05 +0100

    plot_sets is now importabl in other modules

--- plot_sets.py
+++ plot_sets.py
@@ -5,18 +5,6 @@ from matplotlib import animation
 import cost
 
 
-# First set up the figure, the axis, and the plot element we want to animate
-N = 30  # number of sampled points on the path
-W = 101  # image width
-H = 101  # image height
-fig = plt.figure()
-ax = plt.axes(xlim=(0, W-1), ylim=(0, H-1))
-depth_map = cost.load_image('morne_rouge.asc', W, H)
-im = plt.imshow(depth_map, interpolation='nearest')
-gamma = cost.prepare_gamma(np.array([50, 20]), np.array([50, 80]), N)
-c, J_record = cost.cost(depth_map, gamma, 1, 0.25)
-
-
 def color_set(im, J):
     """
     Marks given pixels with NaN on a given image.
@@ -34,21 +22,35 @@ def color_set(im, J):
     return out
 
 
-def animate(i):
+def animate(i, depth_map, gamma, J_record, im, pathplot):
     """
     Animation function. This is called sequentially.
     """
     a = color_set(depth_map, J_record[i])
     im.set_array(a)
-    return [im]
+    gammabis = np.array(gamma)
+    pathplot.set_data(gammabis[:,1], gammabis[:,0])
+    return [im, pathplot]
 
 
-def main():
+def main(filename='morne_rouge.asc', W=101, H=101, N=30, gamma=None,
+        eps_v=1, eps_h=0.25):
     """
     Launches the animation.
     """
+    depth_map = cost.load_image(filename, W, H)
+    if gamma is None:
+        gamma = cost.prepare_gamma(np.array([50, 20]), np.array([50, 80]), N)
+    J_record = cost.cost(depth_map, gamma, eps_v, eps_h)[1]
+
+    # First set up the figure, the axis, and the plot element we want to animate
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, W-1), ylim=(0, H-1))
+    pathplot = ax.plot([], [], '-x')[0]
+    im = plt.imshow(depth_map, interpolation='nearest')
+
     anim = animation.FuncAnimation(fig, animate, frames=N,
-            interval=500, blit=False)
+            fargs=(depth_map, gamma, J_record, im, pathplot), interval=400, blit=False)
     plt.show()
 
 if __name__ == '__main__':
